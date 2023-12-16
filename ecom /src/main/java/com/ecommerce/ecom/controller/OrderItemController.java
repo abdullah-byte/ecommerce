cf7311a634bb98d82acd311a659829f91320af80
Abdullah Mumuni <61237306+abdullah-byte@users.noreply.github.com>
2023-12-16 10:36:18 +0100

    Add files via upload

---
+++ ecom /src/main/java/com/ecommerce/ecom/controller/OrderItemController.java
@@ -0,0 +1,27 @@
+package com.ecommerce.ecom.controller;
+
+
+@RequestMapping("/api/order-items")
+public class OrderItemController {
+    private final OrderItemService orderItemService;
+
+    @Autowired
+    public OrderItemController(OrderItemService orderItemService) {
+        this.orderItemService = orderItemService;
+    }
+
+    @GetMapping
+    public List<OrderItem> getAllOrderItems() {
+        return orderItemService.getAllOrderItems();
+    }
+    @GetMapping("/get/{id}")
+    public Object GetOrderItemById(@PathVariable Long id)
+    {return orderItemService.getOrderItemById(id);}
+
+    @PostMapping
+    public ResponseEntity<OrderItem> createOrderItem(@RequestBody OrderItem orderItem) {
+        OrderItem createdOrderItem = orderItemService.createOrderItem(orderItem);
+        return new ResponseEntity<>(createdOrderItem, HttpStatus.CREATED);
+    }
+
+}
